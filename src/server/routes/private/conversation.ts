38673bcdd53bddbf6123649de6b0cc05afbd01b5
HoJeong Go <seia@outlook.kr>
2023-04-12 18:14:10 +0900

    chore: use sql ident param instead of hard coding

--- src/server/routes/private/conversation.ts
+++ src/server/routes/private/conversation.ts
@@ -22,11 +22,12 @@ export const conversationRouter: FastifyPluginAsyncTypebox = async (fastify, _op
 
 			return db.tx(async t => {
 				// Get conversation by user identifier
+				const userTableName = models.user(t).tableName;
 				const conversationTableName = models.conversation(t).tableName;
 				const conversationMemberTableName = models.conversationMember(t).tableName;
 				const entries = await t.query(t.sql`select c.id, c.flag, c."displayName", c."displayImageUrl", c."updatedAt" from ${t.sql.ident(conversationTableName)} c
 join ${t.sql.ident(conversationMemberTableName)} cm on c.id = cm.conversation
-join "user" u on cm.user = u.id
+join ${t.sql.ident(userTableName)} u on cm.user = u.id
 where u.id = ${request.session.user} and
 c.id >= ${from}
 order by c.id asc limit ${size}`) as Array<Pick<Conversation, 'id' | 'flag' | 'displayName' | 'displayImageUrl' | 'updatedAt'>>;
