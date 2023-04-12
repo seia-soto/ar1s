fe1fd93f6d1e6d09470f1d136116d4e1a2892da7
HoJeong Go <seia@outlook.kr>
2023-04-12 19:37:42 +0900

    feat: patch platform

--- src/server/routes/private/platform/index.ts
+++ src/server/routes/private/platform/index.ts
@@ -1,6 +1,6 @@
-import {type FastifyPluginAsyncTypebox} from '@fastify/type-provider-typebox';
+import {Type, type FastifyPluginAsyncTypebox} from '@fastify/type-provider-typebox';
 import {compileBit} from '../../../../modules/bitwise.js';
-import {db, isFlagExists} from '../../../../modules/database/index.js';
+import {db, isFlagExists, models} from '../../../../modules/database/index.js';
 import {usePermissionError} from '../../../../modules/error.js';
 import {deletePlatform} from '../../../../specs/platform.js';
 import {UserFlags} from '../../../../specs/user.js';
@@ -11,6 +11,26 @@ export const platformRoute: FastifyPluginAsyncTypebox = async (fastify, _opts) =
 		if (!await db.tx(async t => isFlagExists(t, 'user', request.session.user, compileBit(UserFlags.PlatformManager)))) {
 			throw usePermissionError();
 		}
+	});
+
+	fastify.route({
+		url: '/',
+		method: 'PATCH',
+		schema: {
+			body: Type.Object({
+				// eslint-disable-next-line new-cap
+				displayName: Type.Optional(Type.String()),
+				// eslint-disable-next-line new-cap
+				displayImageUrl: Type.Optional(Type.String()),
+			}),
+		},
+		async handler(request, _reply) {
+			return db.tx(async t => {
+				await models.platform(t).update({id: request.session.platform}, request.body);
+
+				return '';
+			});
+		},
 	});
 
 	fastify.route({
