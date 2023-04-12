fcbe6549c9239e823e38cf200093153e8c4601ba
HoJeong Go <seia@outlook.kr>
2023-04-12 18:46:24 +0900

    feat: create another platform

--- src/server/routes/private/bootstrap/index.ts
+++ src/server/routes/private/bootstrap/index.ts
@@ -1,10 +1,12 @@
 import {type FastifyPluginAsyncTypebox} from '@fastify/type-provider-typebox';
-import {compileBit} from '../../../../modules/bitwise.js';
-import {UserFlags} from '../../../../specs/user.js';
+import {Type} from '@sinclair/typebox';
+import {addFlag, compileBit} from '../../../../modules/bitwise.js';
+import {UserFlags, UserFormats} from '../../../../specs/user.js';
 import {db, isFlagExists} from '../../../../modules/database/index.js';
 import {usePermissionError} from '../../../../modules/error.js';
 import {clear} from '../../../../specs/bootstrap.js';
 import {SessionCookieNames} from '../../session/index.js';
+import {PlatformFormats, createPlatform} from '../../../../specs/platform.js';
 
 export const bootstrapRoute: FastifyPluginAsyncTypebox = async (fastify, _opts) => {
 	fastify.addHook('preParsing', async (request, _reply) => {
@@ -13,6 +15,60 @@ export const bootstrapRoute: FastifyPluginAsyncTypebox = async (fastify, _opts) 
 		if (!await db.tx(async t => isFlagExists(t, 'user', request.session.user, bootstrapFlag))) {
 			throw usePermissionError();
 		}
+	});
+
+	fastify.route({
+		url: '/',
+		method: 'POST',
+		schema: {
+			body: Type.Object({
+				platform: Type.Object({
+					inviteIdentifier: Type.String({
+						format: PlatformFormats.InviteIdentifier,
+					}),
+					displayName: Type.String({
+						format: PlatformFormats.DisplayName,
+					}),
+					token: Type.String(),
+				}),
+				user: Type.Object({
+					username: Type.String({
+						format: UserFormats.Username,
+					}),
+					password: Type.String({
+						format: UserFormats.Password,
+					}),
+				}),
+			}),
+		},
+		async handler(request, _reply) {
+			return db.tx(async t => {
+				const {platform: platformParams, user: managerUserParams} = request.body;
+				let managerFlag = 0;
+
+				// We use UserFlags.Bootstrap flag to know if the user is instance manager (global superuser)
+				managerFlag = addFlag(managerFlag, compileBit(UserFlags.Bootstrap));
+				managerFlag = addFlag(managerFlag, compileBit(UserFlags.PlatformManager));
+
+				await createPlatform(
+					t,
+					{
+						...platformParams,
+						displayImageUrl: '',
+					},
+					{
+						...managerUserParams,
+						flag: managerFlag,
+						displayName: '',
+						displayAvatarUrl: '',
+						displayBio: '',
+					},
+					false,
+				);
+
+				return '';
+			});
+		},
 	});
 
 	fastify.route({
