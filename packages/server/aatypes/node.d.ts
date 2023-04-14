694fd14ce27924bc7dc746e9eb5051e9bd702b6a
HoJeong Go <seia@outlook.kr>
2023-04-14 13:17:57 +0900

    feat: keydb with pooling

--- packages/server/aatypes/node.d.ts
+++ packages/server/aatypes/node.d.ts
@@ -6,5 +6,7 @@ declare namespace NodeJS {
 		TOKEN_EXPIRATION: string;
 		TOKEN_TEMPORARY_EXPIRATION: string;
 		TOKEN_RENEWAL_PERIOD: string;
+		KEYDB_POOL_MIN: string;
+		KEYDB_POOL_MAX: string;
 	};
 }
---
+++ packages/server/src/modules/keydb.ts
@@ -0,0 +1,25 @@
+import {createClient} from 'redis';
+import genericPool from 'generic-pool';
+import {shouldEnvBeTypeOrFallback} from './env';
+
+shouldEnvBeTypeOrFallback('KEYDB_POOL_MIN', 2);
+shouldEnvBeTypeOrFallback('KEYDB_POOL_MAX', 10);
+
+const min = parseInt(process.env.KEYDB_POOL_MIN, 10) || 2;
+const max = parseInt(process.env.KEYDB_POOL_MAX, 10) || 10;
+
+export const keydb = genericPool.createPool({
+	async create() {
+		const client = createClient();
+
+		await client.connect();
+
+		return client;
+	},
+	async destroy(resource) {
+		await resource.disconnect();
+	},
+}, {
+	min,
+	max,
+});
