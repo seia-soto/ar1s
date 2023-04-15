716c53c6655cafa391fd30ee5b6be045b6189cc4
HoJeong Go <seia@outlook.kr>
2023-04-15 09:10:01 +0900

    chore: export parcel in spec

--- packages/spec/src/index.ts
+++ packages/spec/src/index.ts
@@ -2,10 +2,13 @@ import * as conversation from './conversation.js';
 import * as conversationMember from './conversationMember.js';
 import * as platform from './platform.js';
 import * as user from './user.js';
+// Live content
+import * as parcel from './parcel.js';
 
 export {
 	conversation,
 	conversationMember,
 	platform,
 	user,
+	parcel,
 };
