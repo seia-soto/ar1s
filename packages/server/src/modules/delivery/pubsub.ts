5a56ff6f58bce186c187be0c20a67ef6a62bc98f
HoJeong Go <seia@outlook.kr>
2023-04-15 09:10:23 +0900

    feat: efficient pubsub on clustering

--- packages/server/src/modules/delivery/pubsub.ts
+++ packages/server/src/modules/delivery/pubsub.ts
@@ -1,3 +1,5 @@
+import {type parcelTypes} from '@ar1s/spec/out/parcel.js';
+import {type Static} from '@sinclair/typebox';
 import {createClient} from 'redis';
 import {keydb} from '../keydb.js';
 import {getLocalPeer, getPeer} from './peer.js';
@@ -8,19 +10,36 @@ const pack = (platform: number, user: number, message: string) => JSON.stringify
 
 const unpack = (intertransactional: string) => JSON.parse(intertransactional) as {platform: number; user: number; message: string};
 
-export const publish = async (platform: number, user: number, message: string) => {
+export const publish = async (platform: number, user: number, message: Static<typeof parcelTypes>) => {
 	const peer = await getPeer(platform, user);
+	const payload = JSON.stringify(message);
 
-	// The peer is connected on remote
-	if (typeof peer.local !== 'undefined') {
-		peer.local.connection.socket.send(message);
-	}
+	const toLocal = async () => {
+		peer.local.connection.socket.send(payload);
+	};
 
-	if (typeof peer.remote === 'string' && peer.remote !== '1') {
+	const toRemote = async () => {
 		const c = await keydb.acquire();
 
-		await c.publish(pubsubNamespace, pack(platform, user, message));
+		await c.publish(pubsubNamespace, pack(platform, user, payload));
 		await keydb.release(c);
+	};
+
+	// The peer is connected on local
+	if (typeof peer.local !== 'undefined') {
+		await toLocal();
+
+		// The peer is connected on local and remote indicates there are more connections
+		if (typeof peer.remote === 'string' && peer.remote !== '1') {
+			await toRemote();
+		}
+
+		return;
+	}
+
+	// The peer is connected on remote
+	if (typeof peer.remote === 'string') {
+		await toRemote();
 	}
 };
 
