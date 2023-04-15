a1a5539afe12d9355dfadf00a063373c01378cb2
HoJeong Go <seia@outlook.kr>
2023-04-15 10:03:26 +0900

    feat: get conversation id from message parcel

--- packages/server/src/modules/delivery/parcel.ts
+++ packages/server/src/modules/delivery/parcel.ts
@@ -1,4 +1,5 @@
-import {clientParcelTypes, parcelTypes} from '@ar1s/spec/out/parcel.js';
+import {ParcelTypes, clientParcelTypes, parcelTypes, type StaticParcelTypes, type messageCreateParcelType, type messageUpdateParcelType, type messageDeleteParcelType} from '@ar1s/spec/out/parcel.js';
+import {type Static} from '@sinclair/typebox';
 import {TypeCompiler} from '@sinclair/typebox/compiler';
 
 // eslint-disable-next-line new-cap
@@ -10,3 +11,20 @@ export const validateClientParcelType = clientParcelTypeChecker.Check;
 const serverParcelTypeChecker = TypeCompiler.Compile(parcelTypes);
 
 export const validateServerParcelType = serverParcelTypeChecker.Check;
+
+type ParcelKindOfMessage = Static<typeof messageCreateParcelType>
+| Static<typeof messageUpdateParcelType>
+| Static<typeof messageDeleteParcelType>;
+
+export const isMessageParcel = (parcel: StaticParcelTypes): parcel is ParcelKindOfMessage =>
+	parcel.type === ParcelTypes.MessageCreate
+  || parcel.type === ParcelTypes.MessageUpdate
+  || parcel.type === ParcelTypes.MessageDelete;
+
+export const getConversationIdFromMessageParcel = (parcel: ParcelKindOfMessage) => {
+	if (parcel.type === ParcelTypes.MessageDelete) {
+		return parcel.payload;
+	}
+
+	return parcel.payload.conversation;
+};
