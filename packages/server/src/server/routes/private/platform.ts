1dcf261835a7a5c38f03f0c91a82bc84840e9bd0
HoJeong Go <seia@outlook.kr>
2023-04-16 12:10:09 +0900

    feat; get all human users in the platform

--- packages/server/src/server/routes/private/platform.ts
+++ packages/server/src/server/routes/private/platform.ts
@@ -1,3 +1,5 @@
+import {UserFlags} from '@ar1s/spec/out/user.js';
+import {addFlag, compileBit} from '@ar1s/spec/out/utils/bitwise.js';
 import {type FastifyPluginAsyncTypebox} from '@fastify/type-provider-typebox';
 import {db, models} from '../../../modules/database/index.js';
 
@@ -11,6 +13,28 @@ export const platformRouter: FastifyPluginAsyncTypebox = async (fastify, _opts) 
 				const platform = await models.platform(t).find({id: request.session.platform}).select('displayName', 'displayImageUrl').oneRequired();
 
 				return platform;
+			});
+		},
+	});
+
+	// Get all human users in the platform
+	fastify.route({
+		url: '/users',
+		method: 'GET',
+		async handler(request, _reply) {
+			return db.tx(async t => {
+				const systemFlag = addFlag(0, compileBit(UserFlags.System));
+				const assistantFlag = addFlag(0, compileBit(UserFlags.Assistant));
+
+				const users = await models.user(t)
+					.find({
+						platform: request.session.platform,
+					})
+					.andWhere(t.sql`not flag & ${systemFlag} = ${systemFlag} or flag & ${assistantFlag} = ${assistantFlag}`)
+					.select('id', 'flag', 'username', 'displayName', 'displayAvatarUrl')
+					.all();
+
+				return users;
 			});
 		},
 	});
