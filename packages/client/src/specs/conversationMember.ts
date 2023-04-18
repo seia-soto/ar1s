fc6e77ff32ed24b740e3a47ae9b9b452c7fc1f3b
HoJeong Go <seia@outlook.kr>
2023-04-18 09:13:45 +0900

    feat: conversationMember dto actions

---
+++ packages/client/src/specs/conversationMember.ts
@@ -0,0 +1,63 @@
+import {type Aris} from '../index.js';
+import {Context} from './_context.js';
+import {type Conversation} from './conversation.js';
+import {type Platform} from './platform.js';
+import {type User} from './user.js';
+
+export type ConversationMemberReflection = {
+	id: ConversationMember['id'];
+	flag: ConversationMember['flag'];
+	platform: Platform['id'];
+	conversation: Conversation['id'];
+	user: User['id'];
+	displayName: ConversationMember['displayName'];
+	displayAvatarUrl: ConversationMember['displayAvatarUrl'];
+	displayBio: ConversationMember['displayBio'];
+	createdAt: string | ConversationMember['createdAt'];
+	updatedAt: string | ConversationMember['updatedAt'];
+};
+
+export class ConversationMember extends Context {
+	readonly id: number & {__type: 'conversationMember.id'};
+	flag: number;
+	displayName: string;
+	displayAvatarUrl: string;
+	displayBio: string;
+	readonly createdAt: Date;
+	updatedAt: Date;
+
+	private readonly _platform: Platform['id'];
+	private readonly _conversation: Conversation['id'];
+	private readonly _user: User['id'];
+
+	constructor(
+		readonly _context: Aris,
+		params: ConversationMemberReflection,
+	) {
+		super(_context, params.id);
+
+		this.id = params.id;
+		this.flag = params.flag;
+		this.displayName = params.displayName;
+		this.displayAvatarUrl = params.displayAvatarUrl;
+		this.displayBio = params.displayBio;
+		this.createdAt = new Date(params.createdAt);
+		this.updatedAt = new Date(params.updatedAt);
+
+		this._platform = params.platform;
+		this._conversation = params.conversation;
+		this._user = params.user;
+	}
+
+	get conversation(): Conversation | ConversationMember['_conversation'] {
+		return this._context.conversations.get(this._conversation) ?? this._conversation;
+	}
+
+	get platform(): Platform | ConversationMember['_platform'] {
+		return this._context.platforms.get(this._platform) ?? this._platform;
+	}
+
+	get user(): User | ConversationMember['_user'] {
+		return this._context.users.get(this._user) ?? this._user;
+	}
+}
