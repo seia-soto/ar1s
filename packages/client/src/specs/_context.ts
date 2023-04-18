8498bc0966298164634ec03dbea47f672ff29776
HoJeong Go <seia@outlook.kr>
2023-04-18 09:22:02 +0900

    feat: series collection

--- packages/client/src/specs/_context.ts
+++ packages/client/src/specs/_context.ts
@@ -26,6 +26,10 @@ export class Collection<T extends Context> {
 	}
 
 	del(index: EnumerableIndex) {
+		if (!this.map[index]) {
+			return false;
+		}
+
 		// eslint-disable-next-line @typescript-eslint/no-dynamic-delete
 		delete this.map[index];
 
@@ -34,5 +38,69 @@ export class Collection<T extends Context> {
 
 	values() {
 		return Object.values(this.map);
+	}
+}
+
+export class Series<T extends Context> extends Collection<T> {
+	arr: T[] = [];
+
+	constructor(
+		readonly threshold = 400,
+	) {
+		super();
+	}
+
+	pop() {
+		const entry = this.arr.pop();
+
+		if (entry) {
+			// eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+			delete this.map[entry._enumerable];
+		}
+	}
+
+	shift() {
+		const entry = this.arr.shift();
+
+		if (entry) {
+			// eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+			delete this.map[entry._enumerable];
+		}
+	}
+
+	add(entry: T) {
+		while (this.arr.length < this.threshold) {
+			this.shift();
+		}
+
+		this.arr.push(entry);
+		this.map[entry._enumerable] = entry;
+
+		return true;
+	}
+
+	del(index: EnumerableIndex) {
+		const entry = this.map[index];
+
+		if (!entry) {
+			return false;
+		}
+
+		for (let i = 0; i < this.arr.length; i++) {
+			if (entry === this.arr[i]) {
+				this.arr.splice(i, 1);
+
+				break;
+			}
+		}
+
+		// eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+		delete this.map[entry._enumerable];
+
+		return true;
+	}
+
+	values() {
+		return this.arr;
 	}
 }
