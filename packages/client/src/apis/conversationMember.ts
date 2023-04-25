5f168eb0735ec664303f57436f4603debeb19ffb
HoJeong Go <seia@outlook.kr>
2023-04-25 13:48:42 +0900

    fix: invalid request url for getConversationMembers

--- packages/client/src/apis/conversationMember.ts
+++ packages/client/src/apis/conversationMember.ts
@@ -2,7 +2,7 @@ import {type ConversationMemberReflection} from '../models/conversationMember.js
 import {type Fetcher} from './aatypes.js';
 
 export const getConversationMembers = async (fetcher: Fetcher, conversationId: number) => {
-	const response = await fetcher('private/conversation/' + conversationId.toString() + '/member', {method: 'get'});
+	const response = await fetcher('private/conversation/' + conversationId.toString() + '/members', {method: 'get'});
 	const reflections: ConversationMemberReflection[] = await response.json();
 
 	return reflections;
